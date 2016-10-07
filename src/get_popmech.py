7d150fd5790aeb38790993dd7582bd7b2275b77b
gaurwaithmelui <strategy155@gmail.com>
2016-10-07 15:41:14 +0300

    finished algo at get

--- src/get_popmech.py
+++ src/get_popmech.py
@@ -18,7 +18,7 @@ def _download_from_refs(list_of_refs,target_path):
     return None
 
 
-def creating_directory_map():
+def _creating_directory_map():
     utils.create_directory_if_needed(DEFAULT_ROOT)
     utils.create_directory_if_needed(DEFAULT_PLAIN_TEXT)
     utils.create_directory_if_needed(DEFAULT_MYSTEM_XML)
@@ -36,10 +36,8 @@ def _extract_link_popmech(div_tag):
     return urllib.parse.urljoin(ROOT_URL,div_tag.a['href'])
 
 
-def main():
-    _mag_tags=_extract_spec_refs_from_page(ARCHIVE_URL,MAG_CLASS_DICT)
-    _mag_refs=_convert_popmech_tags_to_refs(_mag_tags)
-    for mag in _mag_refs:
+def _parsing_mags(refs):
+    for mag in refs:
         magazin_string=str(mag)
         path=utils.produce_path(DEFAULT_PLAIN_TEXT,utils.produce_path(magazin_string.split('/')[-3],magazin_string.split('/')[-2]))
         article_tags=_extract_spec_refs_from_page(mag, ARTICLES_CLASS_DICT)
@@ -47,6 +45,12 @@ def main():
         utils.create_directory_if_needed(path)
         _download_from_refs(article_refs,path)
 
+
+def main():
+    _creating_directory_map()
+    _mag_tags=_extract_spec_refs_from_page(ARCHIVE_URL,MAG_CLASS_DICT)
+    _mag_refs=_convert_popmech_tags_to_refs(_mag_tags)
+    _parsing_mags(_mag_refs)
 
 if __name__ == '__main__':
     main()
